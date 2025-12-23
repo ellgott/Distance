d8f7e70ee871c4af900060058b18c39c6ccbcf48
Ella Jansson <ellajansson@Alfred-Nobel.lan>
2025-12-23 17:26:40 +0100

    -::-

--- search.js
+++ search.js
@@ -18,9 +18,15 @@ document.getElementById("search_bar").addEventListener("submit", function(event)
         const city_s = document.getElementById("city_1").value; // assigns search input variable name
         const city_t = document.getElementById("city_2").value; // second city
 
+        source = search(city_s)
+        const country_s = source.country
+        const lat_s = source.lat
+        const lon_s = source.lon
 
-        const {country_s, lat_s, lon_s} = extract(city_s)
-        const {country_t, lat_t, lon_t} = extract(city_t)
+        target = search(city_t)
+        const country_t = target.country
+        const lat_t = target.lat
+        const lon_t = target.lon
             
         document.getElementById("search_request").textContent = 
         `Distance from ${city_s}, ${country_s} to ${city_t}, ${country_t}`; // changes content of paragraph result
