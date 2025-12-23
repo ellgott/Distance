6bb24eb921a71278aee0c5575168a1494f9834bc
Ella Jansson <ellajansson@Alfred-Nobel.lan>
2025-12-23 16:55:26 +0100

    -::-

--- search.js
+++ search.js
@@ -1,8 +1,17 @@
-import { cities } from "Locations/locs_clean.js";
+import { cities } from "./Locations/locs_clean.js";
 
 function search(city) {
     return cities[city];
 }
+function extract(city) {
+    const result = search(city)
+    if (!result) {
+        return "City does not exist in database"
+    }
+    const {country, lat, lon} = result
+    return { country, lat, lon }  // Returnerar ett objekt med alla tre värden
+}
+
 
 
 document.getElementById("search_bar").addEventListener("submit", function(event) {
@@ -14,19 +23,7 @@ document.getElementById("search_bar").addEventListener("submit", function(event)
         document.getElementById("search_request").textContent = 
         "Du söker avstånd från " + city_s + " till " + city_t; // changes content of paragraph result
 
-
-        
-        const source = search(city_s);
-        const country_s = source.country;
-        const lat_s = source.lat;
-        const lon_s = source.lon;
-
-        const target = search(city_t);
-        const country_t = target.country;
-        const lat_t = target.lat;
-        const lon_t = target.lon;
-
         document.getElementById("search_result").textContent = 
-        city_s + " in " + country_s + " and " + city_t + " in " + country_t;
+        "Soon to be discovered";
 
     }) 
