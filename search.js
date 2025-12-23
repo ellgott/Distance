bbe10dcc5721dc326a6d80eaf3f526631897cc43
Ella Jansson <ellajansson@Alfred-Nobel.lan>
2025-12-23 17:22:59 +0100

    -::-

--- search.js
+++ search.js
@@ -1,29 +1,29 @@
-import { cities } from "./Locations/locs_clean.js";
+import { cities } from "./Locations/locs_clean.js"; // Retrieving my data
 
 function search(city) {
-    return cities[city];
+    return cities[city]; // Collecting data from dic
 }
-function extract(city) {
+function extract(city) { // Extract data
     const result = search(city)
     if (!result) {
         return "City does not exist in database"
     }
     const {country, lat, lon} = result
-    return { country, lat, lon }  // Returnerar ett objekt med alla tre värden
+    return { country, lat, lon }  // Triple object
 }
 
-
-
 document.getElementById("search_bar").addEventListener("submit", function(event) {
-        event.preventDefault(); // Förhindra att sidan laddas om
+        event.preventDefault(); // Page not reloading
 
         const city_s = document.getElementById("city_1").value; // assigns search input variable name
         const city_t = document.getElementById("city_2").value; // second city
 
 
-
+        const {country_s, lat_s, lon_s} = extract(city_s)
+        const {country_t, lat_t, lon_t} = extract(city_t)
+            
         document.getElementById("search_request").textContent = 
-        "Du söker avstånd från " + city_s + " till " + city_t; // changes content of paragraph result
+        `Du söker avstånd från ${city_s}, ${country_s} till ${city_t}, ${country_t}`; // changes content of paragraph result
 
         document.getElementById("search_result").textContent = 
         "Soon to be discovered";
