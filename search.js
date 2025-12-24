e3164632c0413c7c4cf7985b248efc6bd69a44fb
Ella Jansson <ellajanssons@gmail.com>
2025-12-24 10:28:29 +0100

    options

--- search.js
+++ search.js
@@ -10,7 +10,7 @@ const city_names = Object.keys(cities);
 city_names.forEach(city => {
   const option = document.createElement('option');
   option.value = city;
-  datalist.appendChild(option);
+  datalist.appendChild(option.value);
 });
 
 // Follow the writing...
@@ -69,7 +69,7 @@ document.getElementById("search_bar").addEventListener("submit", function(event)
         const target = cities[city_t] // -::-
 
         const distance = haversine(source,target)
-        const part_max = distance / (2*Math.PI*6371)
+        const part_max = (distance / (2*Math.PI*6371)).toFixed(2)
             
         document.getElementById("search_request").textContent = 
         `Distance from ${city_s}, ${source.country} to ${city_t}, ${target.country}`; // changes content of paragraph result
